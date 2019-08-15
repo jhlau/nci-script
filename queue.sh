2f43a5cff8bcd158b06c4ea269cc92a2b362f301
Jey Han Lau <jhlau@doe.cis.unimelb.edu.au>
2019-08-15 13:00:34 +1000

    updating queue.sh

--- queue.sh
+++ queue.sh
@@ -1,10 +1,22 @@
 #/bin/bash
 
+
+
+
+###########################################
+###########################################
+#add the following lines to your job script
+
+#IMPORTANT: you need to define how many child processes that will
+#be created to use the CPU (one child process = 1 CPU). As a rule of thumb, the number of child
+#processes should be half of your requested CPUs.
+number_of_cpu_processes=3
+
+kill_child_process() { for c in $@; do kill $c; done }
+
 #spawn a number of child processes to use up the CPU
 count=0
 children=""
-
-trap "echo $children" EXIT
 
 while (($count<=1))
 do
@@ -17,12 +29,18 @@ do
     count=$(( count+1 ))
 done
 
+#kill off child processes when the main program finishes
+trap "kill_child_process $children" EXIT
+
+###########################################
+###########################################
+
+#PBS -P cp1
+#PBS -q gpu
+#PBS -l walltime=20:00:00
+#PBS -l mem=300MB
+#PBS -l ncpus=6
+#PBS -l ngpus=2
+
 #main job here
 #python mnist.py
-sleep 180
-
-#main job done; kill off the child processes
-for c in $children
-do
-    kill $c
-done
