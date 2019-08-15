d1d016ea44fb9e985f8d2dc0f76355396b3c4898
Jey Han Lau <laujh@4180L-150001-M.local>
2019-08-15 12:28:15 +1000

    minor update

--- queue.sh
+++ queue.sh
@@ -3,10 +3,13 @@
 #spawn a number of child processes to use up the CPU
 count=0
 children=""
-while (($count<=2))
+
+trap "echo $children" EXIT
+
+while (($count<=1))
 do
     #spawn a child process
-    python -c "while True: pass" &
+    nice -19 python -c "while True: pass" &
     #get child process id
     child_pid=$!
     #collect the child processes
@@ -16,6 +19,7 @@ done
 
 #main job here
 #python mnist.py
+sleep 180
 
 #main job done; kill off the child processes
 for c in $children
